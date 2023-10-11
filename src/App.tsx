d0faecda79be6647579e6c7aae0a2ba040522f8c
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-10-11 16:08:14 -0700

    Added a shape-divider component to add some style to how i divide up my sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Home from './pages/Home';
 
 function App() {
-  const [headerHeight, setHeaderHeight] = React.useState(0);
   return (
     <>
-      <Navbar setHeaderHeight={setHeaderHeight} />
-      <main style={{ paddingTop: `${headerHeight}px` }}>
+      <Navbar />
+      <main id="main-content" className="overflow-hidden">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
