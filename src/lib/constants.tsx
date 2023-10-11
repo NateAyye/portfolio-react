e163c0af378c893fc2330f30ad368ac9f9550e73
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-10-11 16:11:36 -0700

    Added icons for the techlologies section in the public folder and made the constants for the technologies with the links to the images and labels

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -16,3 +16,54 @@ export const navbarLinks = [
     href: '/contact',
   },
 ];
+
+export const technologies = [
+  {
+    name: 'Figma',
+    icon: '/images/icons/figma.svg',
+  },
+  {
+    name: 'Github',
+    icon: '/images/icons/github.svg',
+  },
+  {
+    name: 'Notion',
+    icon: '/images/icons/notion.svg',
+  },
+  {
+    name: 'Javascript',
+    icon: '/images/icons/javascript.svg',
+  },
+  {
+    name: 'Jest',
+    icon: '/images/icons/jest.svg',
+  },
+  {
+    name: 'MongoDB',
+    icon: '/images/icons/mongodb.svg',
+  },
+  {
+    name: 'Node',
+    icon: '/images/icons/node.svg',
+  },
+  {
+    name: 'Npm',
+    icon: '/images/icons/npm.svg',
+  },
+  {
+    name: 'React',
+    icon: '/images/icons/react.svg',
+  },
+  {
+    name: 'Sass',
+    icon: '/images/icons/sass.svg',
+  },
+  {
+    name: 'Typescript',
+    icon: '/images/icons/typescript.svg',
+  },
+  {
+    name: 'Webpack',
+    icon: '/images/icons/webpack.svg',
+  },
+];
