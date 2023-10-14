46adb8693398d4cebddac4d96fea4516e1c29d91
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-10-14 14:51:41 -0700

    Making the base path for the repo work with react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppProvider>
       <ThemeProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/portfolio-react">
           <App />
         </BrowserRouter>
       </ThemeProvider>
